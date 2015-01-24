3c7ad769da7ff8c941cc29ab53e9074faffa69c3
Tejas Manohar <me@tejasmanohar.com>
2015-01-24 12:34:12 -0600

    scaffold cli

--- listen-to.js
+++ listen-to.js
@@ -1,3 +1,11 @@
 #!/usr/bin/env node
 
 var program = require('commander');
+
+program
+  .version('0.0.0')
+  .command('listen-to <song title>')
+  .action(function () {
+  });
+
+program.parse(process.argv);
