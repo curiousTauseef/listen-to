a89c7754744973a1888a6d1956766db1d61d16b8
Tejas Manohar <me@tejasmanohar.com>
2015-01-24 12:47:56 -0600

    geturl

--- listen-to.js
+++ listen-to.js
@@ -7,6 +7,25 @@ var program = require('commander');
 var request = require('superagent');
 var youtubedl = require('youtube-dl');
 
+function searchMusic(query, cb) {
+  superagent
+  .get('http://partysyncwith.me:3005/search/'+ query +'/1')
+  .end(function(err, res) {
+    if(err) {
+      console.log(err);
+    } else {
+      if (typeof JSON.parse(res.text).data !== 'undefined') {
+        if (JSON.parse(res.text).data[0].duration < 600) {
+          var url = JSON.parse(res.text).data[0].video_url;
+          cb(url);
+        } else {
+          cb(null);
+        }
+      }
+    }
+  })
+}
+
 program
   .version('0.0.0')
   .command('listen-to <title>')
